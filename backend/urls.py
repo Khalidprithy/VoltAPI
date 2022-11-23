3e6dafa92c17122ba8305aa89cced74e12487441
Ashsenior <ashutoshjunior@gmail.com>
2022-11-23 16:01:38 +0530

    Df

--- backend/urls.py
+++ backend/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('user/create-startup/', CreateStartupView.as_view()),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('user/get-startups/', GetStartupsView.as_view()),
     path('user/get-dashboard-data/', GetStartupView.as_view()),
 ]
