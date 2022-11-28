8ab6af9d5591a47e41302bb9053d4584c52fbb44
Ashsenior <ashutoshjunior@gmail.com>
2022-11-28 22:17:08 +0530

    Fg

--- strategy/urls.py
+++ strategy/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('startup/create-strategy/', CreateStrategyView.as_view()),
     path('startup/get-strategies/', GetStrategiesView.as_view()),
     path('startup/get-strategy/', GetStrategyView.as_view()),
+    path('startup/get-team-members/', GetTeamMembers.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
